54a25dc54cc664c4c145c733769f6b1903d42133
mihalova <dominika.mihalovaa@gmail.com>
2020-06-02 14:56:04 +0200

    -

--- Skuska/RomanNumber.py
+++ Skuska/RomanNumber.py
@@ -1,19 +1,20 @@
 import unittest
 
+
 class RomanNumber():
     value = 3999
     letters = ""
 
-    def __init__(self, romanLetters = ""):        
+    def __init__(self, romanLetters=""):
         if not self.badLetters(romanLetters):
             self.letters = romanLetters
             self.value = 0
 
     def badLetters(self, romanLetters):
         badCharInLetters = not all((c.isalpha() or c.isdigit()) for c in romanLetters)
-        duplicatesInLetters = len(romanLetters)!=(len(set(romanLetters)))
+        duplicatesInLetters = len(romanLetters) != (len(set(romanLetters)))
         if (romanLetters == "") or badCharInLetters or duplicatesInLetters:
-            return True    
+            return True
         return False
 
     def maximum(self):
@@ -23,7 +24,7 @@ class RomanNumber():
         result = "3"
         if (length % 2 == 0):
             result = "8"
-        rest = '9'*((length-1)//2)
+        rest = '9' * ((length - 1) // 2)
         return int(result + rest)
 
     def minimum(self):
@@ -37,7 +38,67 @@ class RomanNumber():
 
     def getValue(self):
         return self.value
-            
+
+    def setRomanNumber(self, romanNumber, letters="IVXLCDM"):
+        spolu = 0
+        i = 0
+        rad = 1000
+        if self.badLetters(letters) or romanNumber == "":
+            return -9999
+        for r in romanNumber:
+            if r.upper() not in letters:
+                return -9999
+        while i < (len(romanNumber)):
+            number1, rad1 = self.getBase(romanNumber[i].upper(), letters)
+            number2, rad2 = self.getBase(romanNumber[i + 1].upper(), letters) if i + 1 < len(romanNumber) else (0, 0)
+            number3, rad3 = self.getBase(romanNumber[i + 2].upper(), letters) if i + 2 < len(romanNumber) else (0, 0)
+            number4, rad4 = self.getBase(romanNumber[i + 3].upper(), letters) if i + 3 < len(romanNumber) else (0, 0)
+
+            if number1 == number2 == number3 == number4:
+                return -9999
+
+            if number1 < number2:
+                if letters.index(romanNumber[i + 1].upper()) - letters.index(romanNumber[i].upper()) > 2:
+                    return -9999
+                if number3 == number1:
+                    return -9999
+                if number1 * 2 == number2:
+                    return -9999
+
+            if number1 == number2:
+                if number1 / 5 in [1, 10, 100, 1000]:
+                    return -9999
+                if number2 == number3 and number4 > number3:
+                    return -9999
+
+            if number1 < number2:
+                spolu += number2 - number1
+                rad = number1
+                i += 2
+            else:
+                if (rad1 > rad):
+                    return -9999
+                spolu += number1
+                rad = number1
+                i += 1
+
+        return spolu
+
+    def getBase(self, char, letters):
+        charInLetters = letters.find(char)
+        rad = int(charInLetters / 2)
+        if (charInLetters % 2) == 0:
+            number = 1
+        else:
+            number = 5
+        number = number * pow(10, rad)
+        return (number, rad + 1)
+
+class RomanNumberWithZero(RomanNumber):
+
+    def __init__(self, romanLetters="OIVXLCDM"):
+        super(RomanNumberWithZero, self).__init__(romanLetters)
+
 
 class TestStringMethods(unittest.TestCase):
 
@@ -53,8 +114,7 @@ class TestStringMethods(unittest.TestCase):
         roman = RomanNumber(" ")
         self.assertEqual(roman.maximum(), 3999)
 
-
-    #Maximum
+    # Maximum
     def test1_max_I(self):
         roman = RomanNumber("I")
         self.assertEqual(roman.maximum(), 3)
@@ -91,12 +151,11 @@ class TestStringMethods(unittest.TestCase):
         roman = RomanNumber("123LCDMQF")
         self.assertEqual(roman.maximum(), 39999)
 
-    
-##    def test1_max_ABCDEFGHIJKLMNOPQRST():
-##        roman = RomanNumber(er roman("ABCDEFGHIJKLMNOPQRST")
-##        self.assertEqual(roman.maximum(), 410065407)
-    
-    #set, get int
+    ##    def test1_max_ABCDEFGHIJKLMNOPQRST():
+    ##        roman = RomanNumber(er roman("ABCDEFGHIJKLMNOPQRST")
+    ##        self.assertEqual(roman.maximum(), 410065407)
+
+    # set, get int
     def test1_I_I_3(self):
         roman = RomanNumber("I")
         roman.setValue(3)
@@ -110,7 +169,7 @@ class TestStringMethods(unittest.TestCase):
     def test1_IVX_10(self):
         roman = RomanNumber("IVX")
         roman.setValue(10)
-        self.assertEqual(roman.getValue(),10)
+        self.assertEqual(roman.getValue(), 10)
 
     def test1_IVX_39(self):
         roman = RomanNumber("IVX")
@@ -148,7 +207,6 @@ class TestStringMethods(unittest.TestCase):
     def test1_xXYy(self):
         roman = RomanNumber("xXYy")
         self.assertEqual(roman.maximum(), 89)
-
 
 
 if __name__ == '__main__':
